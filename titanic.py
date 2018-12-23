5efaff82e1f458c0bfd14430d5a0cb4b5b1f288d
hengweihe <hhwsc@sina.com>
2018-12-23 14:16:01 +0800

    successed

--- titanic.py
+++ titanic.py
@@ -3,8 +3,10 @@ import seaborn as sns
 
 
 def plot():
+    
     df = sns.load_dataset('titanic')
-    fig,axes = plt.subplots(nrow=1,ncols=3,figsize=(15,4))
+
+    fig,axes = plt.subplots(nrows=1,ncols=3,figsize=(15,4))
     df_age=df['age'].dropna()
     sns.distplot(df_age,ax=axes[0])
     sns.countplot(x='sex',hue='alive',data=df,ax=axes[1])
